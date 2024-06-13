265fb311bf25f745ac13dc7cffb97dcc206fbbd6
hall0 <hall0@HALLONI-VIVO>
2024-06-13 13:03:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using CSExercise5_Garage;
+using Newtonsoft.Json;
+using System.IO;
+
+// Read json file where all vehicles are stored
+string vehiclesFile = File.ReadAllText("vehicles.json");
+//Deserialize json into list 
+List<Vehicles> vehicles = JsonConvert.DeserializeObject<List<Vehicles>>(vehiclesFile)!;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("SYSTEM: ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("vehicles.json ");
+Console.ResetColor();
+Console.Write("loaded!\n\n");
+
+
+Console.WriteLine("Welcome to the garage!\n" +
+    "What do you want to do?\n" +
+    //submenu that lists what types and how many
+    "[1] - What vehicles are in the garage?\n" +
+    "[2] - Add & Remove vehicles\n" +
+    "[3] - Search for vehicle\n" +
+    "[4] - Create or Load Garage\n" +
+    "[Q] - Quit");
+string selection = Console.ReadLine()!;
+
+switch (selection)
+{
+    case "1":
+        foreach ()
+        break;
+        
+    case "2":
+
+        break;
+        
+    case "3":
+
+        break;
+    case "4":
+
+        break;
+
+    default:
+        Console.WriteLine("Please input a valid entry");
+        break;
+}
+
---
+++ Vehicles.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSExercise5_Garage
+{
+    public class Vehicles
+    {
+        public required string type { get; set; }
+        public required string model { get; set; }
+        public required string year { get; set; }
+        public required string color { get; set; }
+    }
+
+}
